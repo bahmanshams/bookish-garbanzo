06b6a1094722fbc7c8f1fc4e07177b4f3d3b867a
Bahman Shams <bahmanshams@live.com>
2018-06-14 16:43:04 +0430

    event to broadcast

---
+++ app/Http/Controllers/ChatController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use App\Events\ChatEvent;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+/**
+ * Class ChatController
+ * @package App\Http\Controllers
+ */
+class ChatController extends Controller
+{
+
+    /**
+     * ChatController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+
+    /**
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function chat()
+    {
+        return view('chat');
+    }
+
+
+    /**
+     * @param Request $request
+     */
+    public function send(request $request)
+    {
+        $user = User::find(Auth::id());
+        event(new ChatEvent($request->message, $user));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('chat', function () {
-   return view('chat');
-});
+Route::get('chat', 'ChatController@chat');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
